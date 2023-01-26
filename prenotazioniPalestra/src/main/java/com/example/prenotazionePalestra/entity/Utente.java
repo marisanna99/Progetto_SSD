16fd0e07525ed5626746a887537db3bccdf81046
RobertaDeLuca <72610586+RobertaDeLuca@users.noreply.github.com>
2023-01-26 10:50:35 +0100

    aggiunta username e password

--- prenotazioniPalestra/src/main/java/com/example/prenotazionePalestra/entity/Utente.java
+++ prenotazioniPalestra/src/main/java/com/example/prenotazionePalestra/entity/Utente.java
@@ -1,5 +1,6 @@
 package com.example.prenotazionePalestra.entity;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -30,6 +31,11 @@ public class Utente{
     @NotEmpty(message = "Il campo va compilato")
     private String nome;
     private String cognome;
+    private String username;
+
+    @NotNull
+    @NotEmpty(message = "Il campo va compilato")
+    private String password;
 
     @Email(message = "Email non valida", regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
     @NotEmpty(message = "Email non può essere vuota")

c4eb98b120975acc8737f3612700f089e22be09a
marisanna99 <m.nelli@studenti.unina.it>
2023-02-07 12:10:19 +0100

    delete

--- prenotazioniPalestra/src/main/java/com/example/prenotazionePalestra/repository/PrenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.prenotazionePalestra.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.example.prenotazionePalestra.entity.Prenotazione;
-
-public interface PrenRepository extends CrudRepository<Prenotazione, Integer> {
-
-}
